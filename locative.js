7da8907067dffa375a8dcfbe6419e20f314ed6b0
Robert Twomey <robert.twomey@gmail.com>
2024-03-22 08:08:06 -0500

    Initialize only-search-next

---
+++ locative.js
@@ -0,0 +1,362 @@
+// cleaning the stables | rtwomey@unl.edu | april 25 2023
+// code derived from this test sketch
+// https://editor.p5js.org/robert.twomey/sketches/o0KbIjeGX
+
+// to view full screen
+// https://editor.p5js.org/robert.twomey/full/78R7AgEZp
+
+let simulate = false;
+
+let thismap;
+let v1;
+
+// this variable stores the current position
+let myposition;
+let lastgeoloc = 0;
+let locinterval = 500; // 0.5 sec;
+
+let simposition; // simulated position when clicking
+
+// this is used for distance calculations
+let closest;
+let dist;
+
+let myMap;
+let canvas;
+let myMapDiv;
+
+const mappa = new Mappa("Leaflet");
+// let sounds = [];
+let sounds = {};
+
+let locations = {};
+// let count = 0;
+// let lastHtml = "";
+let locDiv;
+let simulateCheck;
+
+// for adding new points
+let label = "";
+let newPoint = false;
+
+let hidePoint = true;
+
+// leaflet map with google earth tiles
+// https://stackoverflow.com/questions/9394190/leaflet-map-api-with-google-satellite-layer
+// mt0, mt1, mt2, mt3
+
+// rady shell
+let options = {
+  lat: 32.703993,
+  lng: -117.163292,
+  zoom: 20.0,
+  style: 'https://mt0.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
+}
+
+function setupMap() {
+  canvas = createCanvas(500, 1000);
+  // canvas = createCanvas(380, 700);
+  // canvas = createCanvas(800, 800);
+  // canvas = createCanvas(400, 600);
+  // canvas = createCanvas(300, 450);
+  canvas.parent("mapdiv");
+  // canvas.hide();
+
+  // to store position
+  v1 = createVector(width / 2, height / 2);
+
+  // Create a tile map with the options declared
+  myMap = mappa.tileMap(options);
+  myMap.overlay(canvas, hideBadge);
+
+  // DOM element to display locations list
+  // locDiv = select("#locations");
+  getLocation();
+
+  inputDiv= select("#simulate");
+  simulateCheck = createCheckbox('simulate', false);
+  simulateCheck.changed(toggleSimulate);
+  inputDiv.child(simulateCheck);
+
+  textSize(18);
+  
+  updatePosition();
+
+  hideMap();
+}
+
+function hideMap() {
+  myMapDiv = select("#mapdiv");
+  myMapDiv.hide();
+}
+
+function showMap() {
+  myMapDiv = select("#mapdiv");
+  myMapDiv.show();
+}
+
+function draw() {
+  background(220);
+
+  clear();
+  const radyshell = myMap.latLngToPixel(32.79, -117.16);
+  // Using that position, draw an ellipse
+  fill(255, 255, 0);
+  ellipse(radyshell.x, radyshell.y, 5, 5);
+
+  noStroke();
+  if (hidePoint == false) {
+    fill(255, 194, 0);
+    circle(v1.x, v1.y, 10);
+
+    if (myposition != undefined) {
+      const thispxloc = myMap.latLngToPixel(
+        myposition.coords.latitude,
+        myposition.coords.longitude
+      );
+      fill(255, 255, 0);
+      circle(thispxloc.x, thispxloc.y, 10);
+    }
+  }
+  fill(255, 255, 0);
+  circle(mouseX, mouseY, 15);
+
+  if (myposition != undefined && simposition != undefined) {
+    fill(255, 255, 0);
+    text(
+      "geolocation: " +
+        myposition.coords.latitude +
+        " " +
+        myposition.coords.longitude,
+      50,
+      30
+    );
+    text("clicked: " + round(simposition.lat, 6) + " " + round(simposition.lng, 6), 50, 50);
+    text("distance: "+round(dist, 2), 50, 70)
+  }
+
+  for (let p in locations) {
+    const thispxloc = myMap.latLngToPixel(locations[p].lat, locations[p].lng);
+    if (p == closest) {
+      fill(255, 194, 0);
+      circle(thispxloc.x, thispxloc.y, 10);
+      fill(255, 255, 0);
+      text(round(dist, 2), thispxloc.x + 5, thispxloc.y - 15)
+      // console.log("closest point: " + locations[closest].label + " " + dist);
+    }
+    fill(255, 255, 0);
+    circle(thispxloc.x, thispxloc.y, 6);
+    
+    // text(p, thispxloc.x+5, thispxloc.y+5);
+    text(p + ": " + locations[p].label, 
+         thispxloc.x + 5, 
+         thispxloc.y + 5);
+  }
+  if (millis() - lastgeoloc > locinterval) {
+    getLocation();
+    lastgeoloc = millis();
+  }
+}
+
+function toggleSimulate() {
+  simulate = simulateCheck.checked();  
+}
+
+function getLocation() {
+  if (navigator.geolocation) {
+    // get position and process with callback
+    navigator.geolocation.getCurrentPosition(processGeoloc, showError, geooptions);
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+    exit();
+  }
+}
+
+// see high accuracy here: 
+// https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/getCurrentPosition
+
+const geooptions = {
+  enableHighAccuracy: true,
+  timeout: locinterval,
+  maximumAge: 0
+};
+
+function processGeoloc(position) {
+  if (position != undefined) {
+    myposition = position;
+  }
+}
+
+// function error(err) {
+//   console.warn(`ERROR(${err.code}): ${err.message}`);
+// }
+
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      console.log("GEOLOC ERROR: User denied the request for Geolocation.");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      console.log("GEOLOC ERROR: Location information is unavailable.");
+      break;
+    case error.TIMEOUT:
+      console.log("GEOLOC ERROR: The request to get user location timed out.");
+      break;
+    case error.UNKNOWN_ERROR:
+      console.log("GEOLOC ERROR: An unknown error occurred.");
+      break;
+  }
+}
+
+function mouseClicked() {
+  updatePosition();
+}
+
+function updatePosition() {
+  hidePoint = false;
+  getLocation();
+
+  // screen location for simulation
+  v1.x = mouseX;
+  v1.y = mouseY;
+  simposition = myMap.pixelToLatLng(v1.x, v1.y);
+  
+  if (simulate == true) {
+    let results = findClosest(simposition);
+    closest = results[0];
+    dist = results[1];
+    
+    // console.log("updatePosition(): closest point is ", closest, dist, locations[closest]);
+    // playClosestAndVol(closest, dist);
+  }
+}
+
+function playClosestAndVol(closest, dist) {
+  let maxd = 40; //soundmaxdist;  //40.0;
+  let mind = 25; //soundmindist; //15.0
+
+  // mute all sounds
+  Object.keys(sounds).forEach(function (key) {
+    let thesesounds = sounds[key];
+    for (i in thesesounds) {
+      let s = thesesounds[i];
+      s.setVolume(0);
+    }
+  });
+
+  // print(locations[closest]);
+  if (dist < maxd) {
+    for (i in sounds[closest]) {
+      s = sounds[closest][i];
+      if (s.isPlaying() == false) {
+        s.loop();
+      }
+      // set amplitude
+      let capdist = constrain(maxd - dist, 0, mind);
+      let vol = (capdist * capdist) / (mind * mind);
+      s.setVolume(vol);
+    }
+  } else {
+    for (i in sounds[closest]) {
+      s = sounds[closest][i];
+      s.pause();
+    }
+  }
+}
+
+function findClosest(thislatlng) {
+  let mindist = 5000000.0; // too large in feet
+  let closestKey;
+
+  for (p in locations) {
+    // let thisdist = dist(locations[p].lat, locations[p].lng, simposition.lat, simposition.lng);
+    let thisdist = latLngDist(
+      locations[p].lat,
+      locations[p].lng,
+      thislatlng.lat,
+      thislatlng.lng
+    );
+    if (thisdist < mindist) {
+      closestKey = p;
+      mindist = thisdist;
+    }
+    // console.log(thislatlng, locations[p]);
+  }
+
+  return [closestKey, mindist];
+}
+
+function findClosestInList(thislatlng, thislist) {
+  let mindist = 5000000.0; // too large in feet
+  let closestKey = undefined;
+  
+  for (p in locations) {
+    // let thisdist = dist(locations[p].lat, locations[p].lng, simposition.lat, simposition.lng);
+    if(thislist.includes(locations[p].label)) {
+      console.log(p, locations[p].label);
+      let thisdist = latLngDist(
+        locations[p].lat,
+        locations[p].lng,
+        thislatlng.lat,
+        thislatlng.lng
+      );
+      if (thisdist < mindist) {
+        closestKey = locations[p].label;
+        mindist = thisdist;
+      }
+    }
+    // console.log(thislatlng, locations[p]);
+  }
+
+  return [closestKey, mindist];
+}
+
+function latLngDist(lat1, lon1, lat2, lon2) {
+  // calc distance between two lat/long coords in feet
+  
+  lon1 = (lon1 * PI) / 180;
+  lon2 = (lon2 * PI) / 180;
+  lat1 = (lat1 * PI) / 180;
+  lat2 = (lat2 * PI) / 180;
+
+  // Haversine formula
+  let dlon = lon2 - lon1;
+  let dlat = lat2 - lat1;
+  let a = pow(sin(dlat / 2), 2) + cos(lat1) * cos(lat2) * pow(sin(dlon / 2), 2);
+
+  let c = 2 * asin(sqrt(a));
+
+  // Radius of earth in kilometers. Use 3956
+  // for miles
+  let r = 3956; // 6371;
+
+  // calculate the result
+  return c * r * 5280; // in feet
+}
+
+// function loadMapFile(url) {
+//   locations = loadJSON(url, preloadSounds);
+// }
+
+function hideBadge() {
+  myMapDiv = select("#mapdiv");
+  myMapDiv.child(myMap.map.getContainer());
+
+  let badge = selectAll(".leaflet-control-attribution")[0];
+  badge.hide();
+}
+
+function parseStoryLocations() {
+  let j = 1;
+  for (node in story) {
+    if ("lat" in story[node]) {
+      // console.log("location node: ", node);
+      locations[j] = {
+        "lat": story[node].lat,
+        "lng": story[node].lng,
+        "label": node
+      }
+      j = j+1;
+    }
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,359 @@
+// NOTE: this app has migrated to: 
+// https://github.com/roberttwomey/heraklean-app
+
+// weather radio from here: https://editor.p5js.org/robert.twomey/sketches/edH7FDhyw
+
+// LEFTOVERS
+// let title;
+// let start, stop;
+// let startTime, started = false;
+// let timer;
+// let samples;
+// let table;
+
+// advance through screens
+// let screennum = 0;
+let nextbtn;
+let thisState; // current position in story
+
+// user variables
+
+// composition
+let storyfile = "story.json";
+let jsoncontents;
+let story; // structure for story from JSON
+let showtimes; 
+
+// screen 0
+let vid;
+
+// screen 1
+let charsel, chartext, seltext;
+let characters = [
+  'Clio Jones',
+  'Dion Nguyen',
+  'Hera Ramirez',
+  'Leos Lightning',
+  'Demeter Greenberg',
+  'Misael Maelstrom',
+  'Odysseus Mann',
+  'Iason Schmerz',
+  'Zeus Johnson',
+  'Lily Padilla',
+  'Hydra Wiliams',
+  'Eurydice Chen',
+  'Athena Farmer',
+  'Icarus O’brien',
+  'Aphrodite Sumac',
+  'Orion Tillman',
+  'Reaper Brown',
+  'Ariana Omahan',
+  'Atlas Rodriguez',
+  'Gaia Solange'
+];
+
+// screen 2
+let advslider, advtext;
+let socslider, soctext;
+
+// screen 3
+let audiotext, recbtn;
+let speechoutput;
+let bListening = false;
+
+// screen 4
+let waittext, timertext;
+let timeStartExp;
+let waittime = 5*1000;
+
+// soundfiles
+let currSound;
+let audioFiles = {};
+
+
+function preload() {
+  // vid = createImg("clouds2.gif", "video of clouds", "anonymous", );
+  vid = createImg("assets/clouds2.gif");
+  vid.show();
+  vid.parent("wallpaper");
+
+  // story
+  jsoncontents = loadJSON(storyfile, parseShowData);
+}
+
+function setup() {
+  noCanvas();
+
+  // NOT USED
+  // table = new p5.Table();
+  // table.addColumn("time");
+  // table.addColumn("value");
+  // NOT USED
+
+  // create a unique-ish ID, from https://stackoverflow.com/a/53116778
+  
+  let uid = getItem('uid');
+  if (uid === null) {
+    uid = Date.now().toString(36) + Math.floor(Math.pow(10, 12) + Math.random() * 9*Math.pow(10, 12)).toString(36);
+    storeItem("uid", uid);
+  }
+  console.log("---", uid);
+
+  // console.log(story);
+  // screen 0
+  createSplashScreen();
+  
+  // screen 1
+  createCharSelector();  
+  
+  // screen 2
+  createSliders()
+
+  // screen 3
+  createMicCheck();
+  
+  // screen 4
+  createWaiting();
+
+  // screen 5
+  setupRadio();
+  
+  // minimap for debugging
+  setupMap();
+
+  thisState = "splash";
+}
+
+function charSelectEvent() {
+  let myChar = charsel.value();
+  background(200);
+  console.log('Your character is ' + myChar + '!');
+  storeItem('myCharacter', myChar);
+}
+
+function advanceInterface() {
+  // if (thisState == "splash") loadAudioFiles();
+  if (Object.keys(audioFiles).length <= 0) {
+    loadAudioFiles();
+  }
+
+  // if we have not selected a character
+  if (thisState == "character" && charsel.value() == "") {
+    // select one randomly
+    charsel.selected(sample(characters));
+  }
+
+  // stop speaking and listening, just in case
+  stopListening();
+  speechSynth.cancel();
+
+  if (story[thisState].type == "audio" || story[thisState].type == "question") {
+    waitForNextLoc();
+    
+    // if we are into audio story, check location
+    // let thislatlng = {};
+    // if (simulate == false && myposition != undefined) {
+    //   thislatlng.lat = myposition.coords.latitude;
+    //   thislatlng.lng = myposition.coords.longitude;
+    // } else {
+    //   thislatlng.lat = simposition.lat;
+    //   thislatlng.lng = simposition.lng;
+    // }
+    // let results = findClosest(thislatlng);
+    // closest = results[0];
+    // closestlabel = locations[closest].label;
+    // dist = results[1];
+    // // console.log(thislatlng, closest, closestlabel, dist);
+
+    // // if we are close to a valid next choice and within distance threshold
+    // if (story[thisState].next.includes(closestlabel) && dist < story[closestlabel].radius) {
+    //   thisState = closestlabel;
+    //   console.log("--> moved to "+thisState);
+    // }
+  } else {  
+    let nextState = story[thisState].next[0];
+    thisState = nextState;
+    console.log("--> moved to "+thisState);
+  }
+  renderInterface();
+}
+
+function renderInterface() {
+  // clear previous layers
+  hideAll();
+
+  // display appropriate interface
+  if (thisState == "splash") {
+    vid.show();
+    nextbtn.html("start");
+    nextbtn.show();
+  } else if (thisState == "character") {
+    nextbtn.show();
+    nextbtn.html('next');
+    chartext.show();
+    charsel.show();
+
+  } else if (thisState == "preferences") {
+    nextbtn.show();
+    // advtext.show();
+    // advslider.show();
+    // soctext.show();
+    // socslider.show();
+    showPreferences();
+
+  } else if (thisState == "mictest") {    
+    nextbtn.show();
+    audiotext.show();
+    recbtn.show();
+    speechoutput.show();
+    speechoutput.html("");
+
+    audiotext.html(story["mictest"].text);
+    doMicTest();
+
+  } else if (thisState == "waiting") {
+    waittext.html(story["waiting"].text)
+    waittext.show();
+
+    // TODO fix this formatting code. really ugly.
+    timertext.show();
+    timeStartExp = millis() + waittime;
+    waitToStartScript();
+
+  } else if (thisState == "radio") {  
+    showRadio();
+    radiotext.show();
+    let startRandomStation = sample([changeoma, changelax, changelnk, changemm]);
+    startRandomStation();
+
+  } else if (story[thisState].type == "audio") {    
+    chartext.show()
+    chartext.html(charsel.value());
+
+    // play and advance
+    audioFiles[thisState].play();
+    audioFiles[thisState].onended(advanceInterface)
+
+    if (thisState == "onboarding") {
+      showMap();
+    }
+  } else if (story[thisState].type == "question") {
+    chartext.show()
+    chartext.html(charsel.value());
+    recbtn.show();
+    speechoutput.html("")
+    speechoutput.show();
+
+    // play and listen then advance
+    audioFiles[thisState].play();
+    audioFiles[thisState].onended(listenAndAdvance)
+  }
+}
+
+function waitForNextLoc() {
+  
+  let thislatlng = {};
+  if (simulate == false && myposition != undefined) {
+    thislatlng.lat = myposition.coords.latitude;
+    thislatlng.lng = myposition.coords.longitude;
+  } else {
+    thislatlng.lat = simposition.lat;
+    thislatlng.lng = simposition.lng;
+  }
+
+  let results = findClosestInList(thislatlng, story[thisState].next);
+  closestlabel = results[0]
+  // closestlabel = locations[closest].label;
+  dist = results[1];
+  if (closestlabel != undefined && dist < 5000000) {
+    if (dist < story[closestlabel].radius) {
+      thisState = closestlabel;
+      console.log("--> moved to "+thisState);
+      renderInterface();
+    }
+
+  }
+  setTimeout(waitForNextLoc, 500);
+}
+
+function listenAndAdvance() {
+    console.log(`file has finished playing`);
+
+    console.log("... now listening ...");
+    recbtn.style('background-color', 'red');
+    speechoutput.html("(speak now)");
+    speechoutput.style("color", "gray");
+    speechRec.addEventListener('end', () => stopListening());
+    speechRec.start(false, true);
+}
+
+function sample(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function waitToStartScript() {
+  let timeleft = timeStartExp - millis();
+  if (timeleft > 0) {
+    timertext.html(Math.round(timeleft/1000));
+    setTimeout(waitToStartScript, 100)
+  } else {
+    advanceInterface();
+  }
+}
+
+function parseShowData() {
+  story = jsoncontents["story"];
+  showtimes = jsoncontents["showtimes"];
+  // console.log(story);
+  // console.log(showtimes);
+  // console.log(new Date("Fri, 26 Sep 2014 18:30:00 GMT"));
+  let shows = [
+    new Date(2023, 3, 27, 16, 30),
+    new Date(2023, 3, 27, 17, 30),
+    new Date(2023, 3, 27, 18, 30),
+  ];
+  let nextshow = nearestDate(shows);
+  console.log("next show starts: "+shows[nextshow]);
+
+  // load minimap
+  // parseLocations();
+  parseStoryLocations();
+}
+
+function loadAudioFiles() {
+  // load audio files
+  for(idx in story) {
+    // loop over next possibilities for this storypoint
+    let thisNode = story[idx];
+    if ((thisNode.type == "audio") || (thisNode.type == "question")) {
+      audioFiles[idx] = createAudio(thisNode.audio);
+      console.log("loaded ", thisNode.audio);
+    }
+    // audioFile = createAudio(story[thisState].audio);
+  }
+}
+
+// from https://gist.github.com/miguelmota/28cd8999e8260900140273b0aaa57513
+function nearestDate (dates, target) {
+  if (!target) {
+    target = Date.now()
+  } else if (target instanceof Date) {
+    target = target.getTime()
+  }
+
+  let nearest = Infinity
+  let winner = -1
+
+  dates.forEach(function (date, index) {
+    if (date instanceof Date) {
+      date = date.getTime()
+    }
+    let distance = Math.abs(date - target)
+    if (distance < nearest) {
+      nearest = distance
+      winner = index
+    }
+  })
+
+  return winner
+}
